94df990b3a36285bdd1480d947c4e4759c038818
RavenOfNorth <dhaurylenka.job@yandex.by>
2025-01-21 20:35:14 +0300

    update calculator's logic. Fix some bugs

--- estate-assistant/src/app/page.js
+++ estate-assistant/src/app/page.js
@@ -1,17 +1,14 @@
 import Header from "@/app/components/Header/Header";
 import CalculatorWrapper from "@/app/components/Main/CalculatorWrapper";
-import {getParserInfo} from "@/app/parser/Parser";
 
 
 
 export default function Main() {
-    const props ={
-        usdValue: getParserInfo
-    }
+
     return (
         <div className="app">
             <Header />
-            <CalculatorWrapper {...props}/>
+            <CalculatorWrapper />
         </div>
     )
 }
