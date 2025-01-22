b555bc0bb4970593f4007f6c71a9b21258e377ac
RavenOfNorth <dhaurylenka.job@yandex.by>
2025-01-22 21:04:03 +0300

    start works with auth

--- estate-assistant/src/app/pages/loginPage.js
+++ estate-assistant/src/app/pages/loginPage.js
@@ -1,12 +1,43 @@
-import Login from "@/app/components/Auth/Login/Login";
-import Header from "@/app/components/Header/Header";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../../lib/auth/db";
+import { useRouter } from "next/router";
 
-export default function SignIn() {
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const router = useRouter();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            router.push("/dashboard"); // Редирект после успешного входа
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
     return (
-        <div className="app">
-            <Header/>
-            <Login/>
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            {error && <p>{error}</p>}
         </div>
-
-)
+    );
 }
