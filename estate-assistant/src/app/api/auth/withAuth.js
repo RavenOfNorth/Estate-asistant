b6b16ee0076c5331d30a0cc327241abaf03580e2
RavenOfNorth <dhaurylenka.job@yandex.by>
2025-01-30 19:20:49 +0300

    protected Calculator component

---
+++ estate-assistant/src/app/api/auth/withAuth.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "../../../../lib/auth/db";
+
+const withAuth = (WrappedComponent) => {
+    return function ProtectedRoute(props) {
+        const router = useRouter();
+        const [loading, setLoading] = useState(true);
+
+        useEffect(() => {
+            const unsubscribe = onAuthStateChanged(auth, (user) => {
+                if (!user) {
+                    router.push("/login"); // Если НЕ авторизован — редирект
+                }
+                setLoading(false);
+            });
+
+            return () => unsubscribe();
+        }, []);
+
+        if (loading) return <p>Loading...</p>; // Можно заменить на лоадер
+
+        return <WrappedComponent {...props} />;
+    };
+};
+
+
+export default withAuth;
