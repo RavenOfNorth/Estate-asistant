fff27e26b5dc5c4e94d1b38d3d146acd4105174c
RavenOfNorth <dhaurylenka.job@yandex.by>
2025-01-30 22:32:19 +0300

    return header menu

--- estate-assistant/src/app/components/Header/Header.js
+++ estate-assistant/src/app/components/Header/Header.js
@@ -4,8 +4,8 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>SmartEstate</div>
-            {/*<HeaderNavigation />*/}
+            <div className={styles.logo}><a href="/">SmartEstate</a></div>
+            <HeaderNavigation />
         </header>
     );
 }
